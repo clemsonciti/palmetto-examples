ef100bc9f02278f7f66b5d5d31ecd65f1c41896b
Ashwin Srinath <atrikut@clemson.edu>
2017-06-14 14:07:26 -0400

    Update ABAQUS instructions

--- ABAQUS/job.sh
+++ ABAQUS/job.sh
@@ -9,19 +9,25 @@ module add abaqus/6.14
 NCORES=`wc -l $PBS_NODEFILE | gawk '{print $1}'`
 cd $PBS_O_WORKDIR
 
+SCRATCH=/local_scratch/$USER
+
 # SSH into each node and create the scratch directory
+# copy all input files into the scratch directory
 for node in `uniq $PBS_NODEFILE`
 do
-SCRATCH=/local_scratch/$USER
 ssh $node "mkdir $SCRATCH"
+ssh $node "cp $PBS_O_WORKDIR/*.inp $SCRATCH"
 done
 
+cd $SCRATCH
+
 # run the abaqus program, providing the .inp file as input
-abaqus job=abdemo double input=/scratch2/$USER/ABAQUS/boltpipeflange_axi_solidgask.inp scratch=$SCRATCH cpus=$NCORES mp_mode=mpi interactive 
+abaqus job=abdemo double input=$SCRATCH/boltpipeflange_axi_solidgask.inp scratch=$SCRATCH cpus=$NCORES mp_mode=mpi interactive 
 
 # SSH into each node and remove the scratch directory
 for node in `uniq $PBS_NODEFILE`
 do
 SCRATCH=/local_scratch/$USER
+ssh $node "cp -r $SCRATCH/* $PBS_O_WORKDIR"
 ssh $node "rm -rf $SCRATCH"
 done
