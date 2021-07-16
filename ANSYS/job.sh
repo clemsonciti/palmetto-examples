938d9a94e1a994006d0f70880a1899c3caeeabce
clebegu <85566793+clebegu@users.noreply.github.com>
2021-07-16 11:39:36 -0400

    Update job.sh

--- ANSYS/job.sh
+++ ANSYS/job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #PBS -N ANSYSdis
-#PBS -l select=2:ncpus=4:mpiprocs=4:mem=11gb:interconnect=1g
+#PBS -l select=2:ncpus=4:mpiprocs=4:mem=11gb:interconnect=any
 #PBS -l walltime=1:00:00
 #PBS -j oe
 
 module purge
-module add ansys/17.2
+module add ansys202
 
 cd $PBS_O_WORKDIR
 
@@ -19,7 +19,7 @@ do
     ssh $node "cp $PBS_O_WORKDIR/input.txt $SCRATCH"
 done
 
-ansys172 -dir $SCRATCH -j EXAMPLE -s read -l en-us -b -i input.txt -o output.txt -dis -machines $machines -usessh
+ansys202 -dir $SCRATCH -j EXAMPLE -s read -l en-us -b -i input.txt -o output.txt -dis -machines $machines -usessh
 
 sleep 60
 
