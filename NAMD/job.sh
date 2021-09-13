229cab423fb409f642f91d5e23bb6206968a1d94
grigori-yourganov <61881538+grigori-yourganov@users.noreply.github.com>
2021-09-13 12:36:38 -0400

    Update job.sh

--- NAMD/job.sh
+++ NAMD/job.sh
@@ -8,7 +8,7 @@
 module load namd/2.14
 
 cd $PBS_O_WORKDIR
-mpirun -np 2 namd2 +ppn 9 +setcpuaffinity alanin > alanin.output
+mpirun -np 2 namd2 +ppn 9 +setcpuaffinity +idlepoll alanin > alanin.output
 
 # In this example, we use 2 nodes, 10 CPUs per node, 10 GB of RAM per node, 1 GPU per node, and FDR interconect.
 # Feel free to increase the number of nodes. Make sure they match the -np setting after mpirun. For example,
@@ -16,9 +16,8 @@ mpirun -np 2 namd2 +ppn 9 +setcpuaffinity alanin > alanin.output
 # Feel free to increase the number of CPUs; this will need to match the +ppn setting of namd2. For example,
 # requesting "ncpus=20" means that you will have to set "namd2 +ppn 19".
 # In general, the +ppn setting isnumber of CPUs minus one. This is the number of parallel processes per node.
-# Feel free to increase the amount of RAM (see /etc/hardware-table) if needed.
+# Feel free to increase the amount of RAM (see /etc/hardware-table) if needed. Increasing the number og GPUs doesn't seem to help.
 # I recommend keeping mpiprocs set to one.
-# I also recommend using only one GPU (honestly I haven't tried using two GPUs yet).
 # Feel free to set interconnect to hdr (will be faster but you might have to wait in queue and you might get preempted).
 # However, do NOT set interconnect=any, because you want to be sure that the nodes have the same type of interconnect 
 # (otherwise mpirun might not work properly). 
