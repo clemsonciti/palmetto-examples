4842aed6ebd75abc515bd781ec46c605404900b4
Linh B Ngo <lngo@wcupa.edu>
2021-12-09 09:14:29 -0500

    Update main_torch.py

--- PyTorch/distributed_data_parallel/main_torch.py
+++ PyTorch/distributed_data_parallel/main_torch.py
@@ -6,26 +6,8 @@ import torch.distributed as dist
 from argparse import ArgumentParser
 import os
 
-class AE(nn.Module):
-    def __init__(self, **kwargs):
-        super().__init__()
+import train from train_ddp.py
 
-        self.net = nn.Sequential(
-            nn.Linear(in_features=kwargs["input_shape"], out_features=128),
-            nn.ReLU(inplace=True),
-            # small dimension
-            nn.Linear(in_features=128, out_features=128),
-            nn.ReLU(inplace=True),
-            nn.Linear(in_features=128, out_features=128),
-            nn.ReLU(inplace=True),
-            # Recconstruction of input
-            nn.Linear(in_features=128, out_features=kwargs["input_shape"]),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, features):
-        reconstructed = self.net(features)
-        return reconstructed
 
 def train(gpu, args):
     args.gpu = gpu
