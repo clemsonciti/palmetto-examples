9bfc7c626d2ad8ac2e33da42892df776abb76d48
Linh Ngo <lngo@clemson.edu>
2021-12-09 09:02:40 -0500

    merging things ..

--- PyTorch/distributed_data_parallel/train_ddp.py
+++ PyTorch/distributed_data_parallel/train_ddp.py
@@ -1,3 +1,29 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torch.distributed as dist
+
+class AE(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(in_features=kwargs["input_shape"], out_features=128),
+            nn.ReLU(inplace=True),
+            # small dimension
+            nn.Linear(in_features=128, out_features=128),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=128, out_features=128),
+            nn.ReLU(inplace=True),
+            # Recconstruction of input
+            nn.Linear(in_features=128, out_features=kwargs["input_shape"]),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, features):
+        reconstructed = self.net(features)
+        return reconstructed
+
 def train(gpu, args):
     args.gpu = gpu
     print('gpu:',gpu)
