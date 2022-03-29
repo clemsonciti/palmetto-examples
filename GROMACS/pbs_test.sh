bf750d4029b0909e3ffc5366999336505eb3ba66
grigori-yourganov <61881538+grigori-yourganov@users.noreply.github.com>
2022-03-29 09:41:43 -0400

    Update pbs_test.sh

--- GROMACS/pbs_test.sh
+++ GROMACS/pbs_test.sh
@@ -12,7 +12,8 @@ export OMP_NUM_THREADS=4
 
 cd $PBS_O_WORKDIR
 
-# get the number of MPI processes
+# get the total number of MPI processes
 N_MPI_PROCESSES=`cat $PBS_NODEFILE | wc -l`
+echo number of MPI processes is $N_MPI_PROCESSES
 
 mpirun -np $N_MPI_PROCESSES -npernode 2 gmx_mpi mdrun -s nvt_e1-10.tpr -deffnm example
