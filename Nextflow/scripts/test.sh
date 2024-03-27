06580eeab2f40184a0946d59e4acb595296889e8
Zachary Gerstner <zigerst@clemson.edu>
2024-03-27 10:41:43 -0400

    Create test.sh

---
+++ Nextflow/scripts/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+module use /software/containers/biocontainers
+module load modules/qiime2
+mkdir moving_picture_tutorial
+cd ./moving_picture_tutorial
+wget https://data.qiime2.org/2023.9/tutorials/moving-pictures/sample_metadata.tsv
+mkdir emp-single-end-sequences
+cd ./emp-single-end-sequences
+wget https://data.qiime2.org/2023.9/tutorials/moving-pictures/emp-single-end-sequences/barcodes.fastq.gz
+wget https://data.qiime2.org/2023.9/tutorials/moving-pictures/emp-single-end-sequences/sequences.fastq.gz
+cd ..
