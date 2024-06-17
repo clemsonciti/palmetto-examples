c48288fd45ff04db4b63627f0496d4727711cb1b
Zachary Gerstner <zigerst@clemson.edu>
2024-06-17 11:06:42 -0400

    Create job.sh
    update example for slurm

---
+++ COMSOL/SLURM/job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name COMSOL
+#SBATCH --nodes 1
+#SBATCH --ntasks 8
+#SBATCH --cpus-per-task 1 
+#SBATCH --mem 32gb
+#SBATCH --walltime 01:30:00
+
+module purge
+module load comsol/6.2
+
+cd $SLURM_SUBMIT_DIR
+SCRATCH=$TMPDIR
+
+comsol batch -np8 -tmpdir/$SCRATCH -inputfile free_convection.mph -outputfile free_convection_output.mph
